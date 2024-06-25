6aacaa348236769844f72b1ac35fa8e67572bd7a
abdurahmonHard <abdurahmonusmonov633@gmail.com>
2024-06-25 12:30:34 +0500

    first commit

---
+++ data/Products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    name: "Velcro Ballerinas For Girls (Pink)",
+    image: "/images/6.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Velcro Ballerinas Boys && Girls (Blue)",
+    image: "/images/5.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 539,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Sesme Street unisex-Child ELMO Puppet Slipper",
+    image: "/images/4.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 929,
+    countInStock: 5,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    name: "Lace Cacual Boots For Girls (Tan)",
+    image: "/images/3.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 399,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    name: "Lace Wallking Shoes For Girls (Pink)",
+    image: "/images/2.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 49,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Women Red Heels Sandal",
+    image: "/images/1.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 29,
+    countInStock: 9,
+    rating: 6,
+    numReviews: 7,
+  },
+];
+
+export default products;
